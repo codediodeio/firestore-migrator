0b1a14716bca583b2abe41a14b046fdc4c8d634a
Jacques Rowe <jacques@5x5.co.za>
2018-03-31 00:14:59 +0200

    Added nested sub-collection support.

--- src/index.ts
+++ src/index.ts
@@ -9,15 +9,22 @@ import { processFile as processXlsx } from 'excel-as-json';
 
 let fileValue, collValue;
 
+const description = [
+    'Imports JSON, CSV or XLSX data to a Firestore Collection.',
+    'Supports nested sub-collections with an object key prefix, such as "collection:name".'
+];
+
 args
     .version('0.0.2')
     .arguments('<file> <collection>')
-    .description('Imports JSON, CSV or XLSX data to a Firestore Collection.')
+    .description(description.join("\n  "))
     .option('-i, --id [id]', 'Field to use for document ID')
     .option('-m, --merge', 'Merge Firestore documents where insert id exists')
+    .option('-p, --subcollection-prefix [prefix]', 'Sub collection prefix', 'collection')
     .option('-c, --col-oriented', 'XLSX column orientation. Defaults is row orientation')
     .option('-o, --omit-empty-fields', 'XLSX omit empty fields')
     .option('-s, --sheet [#]', 'XLSX Sheet # to import', '1')
+    .option('-l, --log', 'Output document insert paths')
     .action((file, coll) => {
         fileValue = file;
         collValue = coll;
@@ -53,7 +60,6 @@ async function migrate() {
         const file: string = fileValue;
     
         // Create a batch to run an atomic write
-        const colRef = db.collection(colPath);
         const batch = db.batch();
     
         let data;
@@ -73,13 +79,32 @@ async function migrate() {
             throw "Unknown file extension. Supports .json, .csv or .xlsx!";
         }
     
-        const mode = (data instanceof Array) ? 'array' : 'object';
+        
+        const processCollection = (data: JSON, path: string) => {
+            const mode = (data instanceof Array) ? 'array' : 'object';
+            const colRef = db.collection(path);
+            _.forEach(data, (item:object,id) => {
+                // doc-id preference: object key, invoked --id option, auto-id
+                id = (mode === 'object') ? id : (args.id && _.hasIn(item, args.id)) ? item[args.id].toString() : colRef.doc().id;
+                
+                // Look for and process sub-collections
+                const subColKeys = Object.keys(item).filter(k => k.startsWith(args.subcollectionPrefix+':'));
+                subColKeys.forEach((key:string) => {
+                    const subPath = [path, id, key.slice(1 + args.subcollectionPrefix.length) ].join('/');
+                    processCollection(item[key], subPath);
+                    delete item[key];
+                });
+                
+                // set document path/id and data
+                const docRef = colRef.doc(id);
+                batch.set(docRef, item, { merge: !!(args.merge) });
 
-        _.forEach(data, (item,id) => {
-            id = args.id ? item[args.id].toString() : (mode === 'object') ? id : colRef.doc().id;
-            const docRef = colRef.doc(id);
-            batch.set(docRef, item, { merge: !!(args.merge) });
-        });
+                // log if requested
+                args.log && console.log(docRef.path);
+            });
+        }
+
+        processCollection(data, colPath);
     
         // Commit the batch
         await batch.commit();
@@ -88,7 +113,7 @@ async function migrate() {
     } catch (error) {
         console.log("Migration failed!", error);
     }
-} 
+}
 
 function readCSV(path): Promise<any> {
     return new Promise((resolve, reject) => {
